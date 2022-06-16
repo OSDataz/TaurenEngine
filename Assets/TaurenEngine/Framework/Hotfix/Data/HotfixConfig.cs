d56dab65e33fd9b0be8953e317832944f1f19b9f
Home\Osdataz <342522411@qq.com>
2022-06-16 10:16:42 +0800

    导入Excel和Zip插件工具；调整热更静态函数

---
+++ Assets/TaurenEngine/Framework/Hotfix/Data/HotfixConfig.cs
@@ -0,0 +1,14 @@
+﻿/*┌────────────────────────┐
+ *│　Engine  ：TaurenEngine
+ *│　Author  ：Osdataz
+ *│　Version ：v0.5.0
+ *│　Time    ：2022/6/11 11:52:39
+ *└────────────────────────┘*/
+
+namespace TaurenEngine.Framework
+{
+	public class HotfixConfig
+	{
+
+	}
+}
