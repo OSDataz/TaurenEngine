123f5b7573460054bd3918b71228fbc71cd9ad41
Bert <342522411@qq.com>
2022-07-06 11:03:03 +0800

    调整了GIT项目结构；将热更框架和本地引擎框架放进一个GIT项目管理

---
+++ Assets/TaurenEngine.Hotfix/Framework.Hotfix/Module/View/UIBase.cs
@@ -0,0 +1,13 @@
+﻿/*┌────────────────────────┐
+ *│　Engine  ：TaurenEngine
+ *│　Author  ：Osdataz
+ *│　Version ：v0.6.0
+ *│　Time    ：2022/6/26 19:15:52
+ *└────────────────────────┘*/
+
+namespace TaurenEngine.Hotfix
+{
+	public class UIBase
+	{
+	}
+}
---
+++ Assets/TaurenEngine.Hotfix/Framework.Hotfix/Module/View/ViewBase.cs
@@ -0,0 +1,13 @@
+﻿/*┌────────────────────────┐
+ *│　Engine  ：TaurenEngine
+ *│　Author  ：Osdataz
+ *│　Version ：v0.6.0
+ *│　Time    ：2022/6/26 19:15:46
+ *└────────────────────────┘*/
+
+namespace TaurenEngine.Hotfix
+{
+	public class ViewBase
+	{
+	}
+}
